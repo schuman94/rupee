5f1aeba565c54914b22264ca42e12df4ab9b6318
schuman94 <sergio.chulian@iesdonana.org>
2023-03-31 10:21:17 +0200

    pendiente solucion update database

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
 import subprocess
 import tkinter as tk
-from tkinter import ttk, messagebox
+from tkinter import ttk, messagebox, filedialog
 
 def run_rupee(search_mode, search_type):
     script_path = "./rupee-search/target/run_rupee-search.sh"
     subprocess.run([script_path, search_mode, search_type])
     messagebox.showinfo("Rupee", "La búsqueda ha finalizado.")
 
+def update_database(database_path):
+    database_path = database_path
+
+    if not database_path:
+        messagebox.showerror("Error", "Por favor, selecciona un directorio.")
+        return
+
+    constants_file_path = "src/main/java/edu/umkc/rupee/search/lib/Constants.java"
+    new_dir_path_line = f'    public static String DIR_PATH = "{database_path}";\n'
+
+    with open(constants_file_path, "r") as file:
+        lines = file.readlines()
+
+    with open(constants_file_path, "w") as file:
+        for line in lines:
+            if line.strip().startswith("public static String DIR_PATH"):
+                file.write(new_dir_path_line)
+            else:
+                file.write(line)
+
+    script_path = "./actualizar_bbdd.sh"
+    subprocess.run([script_path])
+    messagebox.showinfo("Rupee", "La base de datos se ha actualizado.")
+
+
 def submit_form():
-    #query = query_entry.get()
     search_mode = search_mode_var.get()
     search_type = search_type_var.get()
 
@@ -18,11 +42,34 @@ def submit_form():
 
     run_rupee(search_mode, search_type)
 
+def browse_directory():
+    directory = filedialog.askdirectory()
+    database_path_var.set(directory)
+
+def update_database_button():
+    database_path = database_path_var.get()
+    if not database_path:
+        messagebox.showerror("Error", "Por favor, selecciona un directorio.")
+        return
+
+    update_database(database_path)
+
 app = tk.Tk()
 app.title("Rupee GUI")
+#app.geometry("600x300")
 
-query_entry = tk.Entry(app)
-query_entry.grid(column=1, row=0)
+database_label = tk.Label(app, text="Base de datos:")
+database_label.grid(column=0, row=0)
+
+database_path_var = tk.StringVar()
+database_entry = tk.Entry(app, textvariable=database_path_var)
+database_entry.grid(column=1, row=0)
+
+browse_button = tk.Button(app, text="Examinar", command=browse_directory)
+browse_button.grid(column=2, row=0)
+
+update_button = tk.Button(app, text="Actualizar", command=update_database_button)
+update_button.grid(column=3, row=0)
 
 search_mode_label = tk.Label(app, text="SEARCH_MODE:")
 search_mode_label.grid(column=0, row=1)
